940dd969c4a15315fe0400226c2be530ea7faeb5
Matteo <rosella.matteo@gmail.com>
2020-04-09 00:41:14 +0200

    aggiustamenti

--- script.js
+++ script.js
@@ -6,39 +6,9 @@ var ricontact = $('.ricontact');
 var z = 0;
 
 $('.tlg').click(
-  function(){
-    $('.mic').removeClass('none');
-    $('.tlg').addClass('none');
-    var text = input.val();
-    chat.append('<div class="chatmsg"><span class="msgsend">' + text + '</span><br><span class="msgtime">23:44</span></div>');
-    $('.msg').val('');
-
-    if (z === 0) {
-      setTimeout(function() {
-        chat.append('<div class="pcmsg"><span class="msgsend">Non mi interessa!</span><br><span class="msgtime">23:44</span></div>')
-        z = z + 1;
-      }, 1000);
-    }
-    else if (z === 1){
-      setTimeout(function() {
-        chat.append('<div class="pcmsg"><span class="msgsend">Ancora che mi scrivi? Non mi interessa!</span><br><span class="msgtime">23:44</span></div>')
-        z = z + 1;
-      }, 1000);
-    }
-    else if (z === 2){
-      setTimeout(function() {
-        chat.append('<div class="pcmsg"><span class="msgsend">Senti basta, ora ti scrivo solo più "ok" almeno sei felice</span><br><span class="msgtime">23:44</span></div>')
-        z = z + 1;
-      }, 1000);
-    }
-    else if (z > 2){
-      setTimeout(function() {
-        chat.append('<div class="pcmsg"><span class="msgsend">Ok</span><br><span class="msgtime">23:44</span></div>')
-      }, 1000);
-    }
-  }
+  message
 )
-
+$('')
 
 $("body").click(function() {
   $('.mic').removeClass('none');
@@ -73,3 +43,33 @@ $('.search').click(
     $('.ricontact').val('');
   }
 )
+function message(){
+  $('.mic').removeClass('none');
+  $('.tlg').addClass('none');
+  var text = input.val();
+  chat.append('<div class="chatmsg"><span class="msgsend">' + text + '</span><br><span class="msgtime">23:44</span></div>');
+  $('.msg').val('');
+  if (z === 0) {
+    setTimeout(function() {
+      chat.append('<div class="pcmsg"><span class="msgsend">Non mi interessa!</span><br><span class="msgtime">23:44</span></div>')
+      z = z + 1;
+    }, 1000);
+  }
+  else if (z === 1){
+    setTimeout(function() {
+      chat.append('<div class="pcmsg"><span class="msgsend">Ancora che mi scrivi? Non mi interessa!</span><br><span class="msgtime">23:44</span></div>')
+      z = z + 1;
+    }, 1000);
+  }
+  else if (z === 2){
+    setTimeout(function() {
+      chat.append('<div class="pcmsg"><span class="msgsend">Senti basta, ora ti scrivo solo più "ok" almeno sei felice</span><br><span class="msgtime">23:44</span></div>')
+      z = z + 1;
+    }, 1000);
+  }
+  else if (z > 2){
+    setTimeout(function() {
+      chat.append('<div class="pcmsg"><span class="msgsend">Ok</span><br><span class="msgtime">23:44</span></div>')
+    }, 1000);
+  }
+}
